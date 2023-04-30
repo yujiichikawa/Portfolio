2c9d8c8628e679306d615712ef2d122278f27bdf
yujiichikawa <thiagogelinski20@gmail.com>
2023-04-30 14:04:33 -0300

    Alterando o component projectscontainer

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,7 +3,7 @@ import CardFlip from "./CardFlip"
 const ProjectsContainer = () => {
   return (
     <section className='projects-container'>
-      <h2>Projetos</h2>
+      <h2>Projetos Web</h2>
       <CardFlip />      
       <a href="https://github.com/yujiichikawa" className='btn'>
         Ver GitHub
